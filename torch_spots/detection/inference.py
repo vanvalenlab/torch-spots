73d31ce63748cee3217746dd67bd2c1b3509db73
Sam Holtzen <samholtzen@gmail.com>
2026-08-12 06:45:46 -0700

    fixed a bug when specifying model path and added tensorboard to dependencies

--- torch_spots/detection/inference.py
+++ torch_spots/detection/inference.py
@@ -38,6 +38,8 @@ class SpotDetection():
             self.model_path = sorted(
                 glob.glob(str(canonical_path / "torch-spots*.pth"))
             )[-1]
+        else:
+            self.model_path = self.model_path
         
         checkpoint = torch.load(self.model_path, map_location=self.device)
         self.model.load_state_dict(checkpoint)
